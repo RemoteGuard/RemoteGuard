43c0a461c2119b31024f85a84966308c6673cc2f
raulreis123 <raulgomesreis@gmail.com>
2024-08-22 23:39:22 -0300

    Configurado a view para o servidor exibir o site

--- site/src/server/server.js
+++ site/src/server/server.js
@@ -1,23 +1,25 @@
 var express = require("express");
 var cors = require("cors");
 let porta = 3303;
-
+let usuarioRouter = require("./routes/usuarios");
+let indexRouter = require("./routes/index");
+let path = require("path");
 // É necessário instalar as bibliotecas EXPRESS e CORS para poder inicializar o servidor!!!
 // Não se esqueça de instalar a biblioteca mysql2 para executar a query com o Banco!
 
 var app = express();
 
-// var usuarioRouter = require("./src/routes/usuarios");
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, "public")));
-
 app.use(cors());
 
-// app.use("/", indexRouter);
-// app.use("/usuarios", usuarioRouter);
+app.use("/", indexRouter);
+app.use("/usuarios", usuarioRouter);
+
+// Configurando a view para que o servidor consiga renderizar o site construido e exibir ao usuário
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../../public")));
+
 
 app.listen(porta, ()=> {
-    console.log('Servidor RemoteGuard rodando!');
+    console.log("Servidor rodando");
 });
