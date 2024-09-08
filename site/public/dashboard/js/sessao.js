252550e601b4ad5cfcc58572d04a23c1c37ba4b6
raulreis123 <raulgomesreis@gmail.com>
2024-09-08 13:51:48 -0300

    Finalização de responsividade tela de cadastro

--- site/public/dashboard/js/sessao.js
+++ site/public/dashboard/js/sessao.js
@@ -3,8 +3,6 @@
 function alterarSidebar(){
     var burguer = document.getElementById('burguer');
     var sidebar = document.getElementById('sidebar');
-    sidebar = sidebar ?? document.querySelector('.headerBarResp');
-    console.log(sidebar)
 
     burguer.classList.toggle('ativo')
     sidebar.classList.toggle('ativoSide')
