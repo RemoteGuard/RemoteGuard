1f676f8af7f96a5814781e2aaf2829cb0bb605af
m-de-mello <murillo.mello@sptech.school>
2024-09-17 01:15:56 -0300

    Atualizando Python | Mudando Biblioteca para Conexão com BD

---
+++ capturaDeDados/config_capture.py
@@ -0,0 +1,103 @@
+import psutil # Captura de Dados de Máquina.
+import time # Funções Temporais -> Discretização.
+import pymysql # Conexão com Banco de Dados.
+import os # Identificação do Sistema Operacional da Máquina.
+import socket # Captura do Nome da Máquina (Hostname).
+
+def create_db_connection(): # Função para Criar a Conexão com Banco de Dados.
+    try:
+        conn = pymysql.connect( # Conexão com o Banco de Dados.
+                host="localhost",
+                user="",
+                password="",
+                database="remote_guard"
+            )
+        cursor = conn.cursor() # Cursor (Objeto) para Executar Comandos SQL.
+        print(f"Conexão com o Banco {conn.db.decode()} estabelecida com Sucesso!")
+        return conn, cursor
+    except pymysql.MySQLError as e:
+        print(f"Falha ao estabelecer Conexão com o Banco de Dados: {e}")
+        return None, None
+
+conn, cursor = create_db_connection() # Executa a Função para Criação da Conexão com o Banco de Dados e Armazena nas Variáveis.
+        
+def get_hostname():
+    return socket.gethostname()
+
+def get_root_directory():
+    return 'C:\\' if os.name == 'nt' else '/'
+
+def verify_equipment_registration():
+    global fkNotebook
+    hostname = get_hostname()
+    # hostname = 'hostTeste'
+    
+    sql = "SELECT idNotebook, hostname FROM notebook WHERE hostname = %s"
+    cursor.execute(sql, (hostname,))
+    register = cursor.fetchone()
+
+    if register: 
+        fkNotebook = register[0]
+    else:
+        sql = "INSERT INTO notebook (hostname) VALUES (%s)"
+        cursor.execute(sql, (hostname,))
+        conn.commit()
+        fkNotebook = cursor.lastrowid
+        
+    return fkNotebook
+        
+def get_process_iter():
+    return psutil.process_iter()
+
+def get_cpu_data():
+    cpu_data = psutil.cpu_times()._asdict()
+    cpu_idle_time = cpu_data['idle']
+    cpu_usage_percentage = psutil.cpu_percent(interval = 1)
+    return cpu_idle_time, cpu_usage_percentage
+
+def get_ram_data():
+    ram_data = psutil.virtual_memory()._asdict()
+    ram_usage_gigabytes = round((ram_data['used'] / pow(10, 9)), 2) # Converte para Gigabytes e Arredonda para duas Casas Decimais.
+    ram_usage_percentage = ram_data['percent']
+    return ram_usage_gigabytes, ram_usage_percentage
+
+def get_disk_data():
+    disk_data = psutil.disk_usage(get_root_directory())._asdict()
+    disk_usage_gigabytes = round((disk_data['used'] / pow(10, 9)), 2)
+    disk_usage_percentage = disk_data['percent']
+    return disk_usage_gigabytes, disk_usage_percentage
+
+# Lista contendo os Processos Ignorados pela Captura.
+list_ignored_processes = [
+    'svchost.exe',
+    'SamsungSARMode.exe',
+    'IntelAnalyticsService.exe' ,
+    'IntelConnectivityNetworkService.exe',
+    'IntelAudioService.exe',
+    'Intel_PIE_Service.exe',
+    'IntelCpHDCPSvc.exe',
+    'IntelConnectService.exe',
+    'IntelConnect.exe',
+    'conhost.exe',
+    'System Idle Process' ,
+    'SystemPlatformEngine.exe',
+    'SamsungSystemSupportService.exe',
+    'SecurityHealthSystray.exe',
+    'SamsungSystemSupportEngine.exe',
+    'SystemSettingsBroker.exe' ,
+    'IntelCpHDCPSvc.exe',
+    'System',
+    'Registry',
+    'dllhost.exe',
+    'services.exe',
+    'wininit.exe',
+    'lsass.exe',
+    'RbackgroundTaskHost.exe',
+    'idea64.exe',
+    'sServiceKeyMonitor.exe',
+    'WindowsMCFCore.exe',
+    'SamsungSettingsHost.exe',
+    'Widgets.exe',
+    'FileSyncHelper.exe',
+    'SamsungSystemSupportEngine.exe'
+]
---
+++ capturaDeDados/main_capture.py
@@ -0,0 +1,46 @@
+from config_capture import conn, cursor, get_hostname, get_root_directory, verify_equipment_registration, get_process_iter, get_cpu_data, get_ram_data, get_disk_data, list_ignored_processes
+import time
+
+def data_capture(processes_monitoring_delay, data_capture_delay):
+    cont_time = 1
+    cont_registers = 1
+    run_data_capture = True
+    
+    while run_data_capture:
+        if(cont_time % processes_monitoring_delay == 0) or cont_time == 1: # Insere os Dados no Banco de acordo com a Discretização.        
+            for process in get_process_iter():
+                process_name = process.name()
+                
+                if(process_name not in list_ignored_processes and process.status() == 'running'):
+                    sql = "INSERT INTO processos (nomeProcesso, fkNotebook) values (%s,%s);"
+                    val = (process_name, fkNotebook)
+                    cursor.execute(sql, val)
+                    conn.commit()
+                                      
+      
+        if cont_time % data_capture_delay == 0 or cont_time == 1: # Insere os Dados no Banco de acordo com a Discretização.   
+            # Chamada para Funções de Captura dos Componentes:
+            cpu_idle_time, cpu_usage_percentage = get_cpu_data()
+            ram_usage_gigabytes, ram_usage_percentage = get_ram_data()
+            disk_usage_gigabytes, disk_usage_percentage = get_disk_data()
+            
+            sql = "INSERT INTO dados (tempoInatividade, percCPU, usedRAM, percRAM, usedDisk, percDisk, fkNotebook) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+            val = (cpu_idle_time, cpu_usage_percentage, ram_usage_gigabytes, ram_usage_percentage, disk_usage_gigabytes, disk_usage_percentage, fkNotebook)
+            cursor.execute(sql, val)
+            conn.commit()
+            
+            print(cont_registers, " Registro Inserido.") if cont_registers == 1 else print(cont_registers, " Registro Inseridos.")
+            cont_registers += 1       
+               
+        time.sleep(1)
+        cont_time+= 1
+
+
+def menu(): # Menu para Configuração da Discretização.
+  processes_monitoring_delay = int(input('Deseja Monitorar os Processos a cada quantos Segundos?: '))
+  data_capture_delay = int(input('Deseja Capturar os Dados a cada quantos Segundos?: '))
+  data_capture(processes_monitoring_delay, data_capture_delay) # Executa a Função para Capturar os Dado.
+
+fkNotebook = verify_equipment_registration() # Verifica se a Máquina já está Registrada no Banco de Dados.
+
+menu() # Executa a Função para Configurar a Captura.
