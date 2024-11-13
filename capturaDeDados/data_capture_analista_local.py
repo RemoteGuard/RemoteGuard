4dcec4b72c063b8244fd7dedc5e02c3f852516ae
RenanPacanaro <renan.trinca@sptech.school>
2024-11-13 20:44:31 -0300

    card que demonstra funcionarios que possuem as maquinas em alerta criado

--- capturaDeDados/data_capture_analista_local.py
+++ capturaDeDados/data_capture_analista_local.py
@@ -8,7 +8,7 @@ import pymysql
 # from botocore.exceptions import ClientError
 #from atlassian import Jira
 #from requests import HTTPError
-cleber = 3
+cleber = 4
 processos_indevidos = [
     # Jogos populares
     "fortnite.exe", "pubg.exe", "leagueoflegends.exe", "valorant.exe", "apex_legends.exe",
