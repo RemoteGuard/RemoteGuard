4c1240de6993ad5d1154d5754c8bd089fbc081b9
RenanPacanaro <renan.trinca@sptech.school>
2024-11-24 22:33:34 -0300

    tela de cadastro de empresas criada e funcional

--- web-data-viz/src/controllers/empresaController.js
+++ web-data-viz/src/controllers/empresaController.js
@@ -15,6 +15,34 @@ function listar(req, res) {
 }
 
 
+function cadastrarEmpresa(req, res) {
+  var razaoSocial = req.body.razaoSocialServer;
+  var nomeFantasia = req.body.nomeFantasiaServer;
+  var cep = req.body.cepServer;
+  var numero = req.body.numeroServer;
+  var telefone = req.body.telefoneServer;
+  var email = req.body.emailServer;
+  var cnpj = req.body.cnpjServer;
+  var token = req.body.tokenServer;
+
+    empresaModel.cadastrarEmpresa(razaoSocial, nomeFantasia, cep, numero, telefone, email, cnpj, token)
+      .then(
+        function (resultado) {
+          res.json(resultado);
+        }
+      ).catch(
+        function (erro) {
+          console.log(erro);
+          console.log(
+            "\nHouve um erro ao realizar o cadastro! Erro: ",
+            erro.sqlMessage
+          );
+          res.status(500).json(erro.sqlMessage);
+        }
+      );
+  }
+
 module.exports = {
-  listar
+  listar,
+  cadastrarEmpresa
 };
--- web-data-viz/src/routes/empresas.js
+++ web-data-viz/src/routes/empresas.js
@@ -2,8 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 var empresaController = require("../controllers/empresaController");
+
 router.post("/listar", function (req, res) {
     empresaController.listar(req, res);
+})
+
+router.post("/cadastrarEmpresa", function (req, res) {
+    empresaController.cadastrarEmpresa(req, res);
 })
 
 module.exports = router;   
