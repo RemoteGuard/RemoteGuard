e8804119c2965ebcb011445cca2cf09733b61973
isaggoulart <isabela.goulart@sptech.school>
2024-12-03 12:42:47 -0300

    NEW: DASHBORAD DO ANALISTA FINALIZADA
    Alterações no grafico de barras , colocando a sessionStorege como fkBase

--- web-data-viz/src/models/renanModel.js
+++ web-data-viz/src/models/renanModel.js
@@ -7,26 +7,40 @@ function processos() {
   }
 
 function ultimosDados() {
-    var instrucaoSql = `SELECT funcionario.nome, t1.fkNotebook, t1.porcentagem_cpu, t1.porcentagem_ram
+    var instrucaoSql = ` SELECT funcionario.nome, t1.fkNotebook, t1.porcentagem_cpu, t1.porcentagem_ram
     FROM dados AS t1
     JOIN (
     SELECT fkNotebook, MAX(id) AS max_id
     FROM dados
     GROUP BY fkNotebook
     ) AS t2 ON t1.fkNotebook = t2.fkNotebook AND t1.id = t2.max_id
-    JOIN funcionario where t2.fkNotebook = funcionario.idFuncionario;`;
+    JOIN notebook 
+    ON t2.fkNotebook = notebook.idNotebook
+JOIN funcionario 
+    ON funcionario.fkNotebook = notebook.idNotebook;`;
     return database.executar(instrucaoSql);
   }
 
  function plotarGrafico() {
-  var instrucaoSql = `SELECT funcionario.nome, t1.processos, t1.fkNotebook, t1.porcentagem_cpu, t1.porcentagem_ram
-    FROM dados AS t1
-    JOIN (
+  var instrucaoSql = `SELECT 
+    funcionario.nome, 
+    t1.processos, 
+    t1.fkNotebook, 
+    t1.porcentagem_cpu, 
+    t1.porcentagem_ram
+FROM 
+    dados AS t1
+JOIN (
     SELECT fkNotebook, MAX(id) AS max_id
     FROM dados
+    WHERE fkNotebook IS NOT NULL
     GROUP BY fkNotebook
-    ) AS t2 ON t1.fkNotebook = t2.fkNotebook AND t1.id = t2.max_id
-    JOIN funcionario where t2.fkNotebook = funcionario.idFuncionario;`;
+) AS t2 
+    ON t1.fkNotebook = t2.fkNotebook AND t1.id = t2.max_id
+JOIN notebook 
+    ON t2.fkNotebook = notebook.idNotebook
+JOIN funcionario 
+    ON funcionario.fkNotebook = notebook.idNotebook;`;
   
     return database.executar(instrucaoSql);
   }
